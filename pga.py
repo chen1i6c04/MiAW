7606ae06a451c7c104b829a2207eac626f05ba80
chen1i6c04 <chen1i6c04@gmail.com>
2020-10-15 13:54:11 +0800

    simplify code

--- pga.py
+++ pga.py
@@ -1,11 +1,13 @@
 import os
+import yaml
+import shutil
 import argparse
-import subprocess
 from tempfile import TemporaryDirectory
 from src.logs import create_logger
-from src.cmd import FastqcCommandline, KmerFinderCommandline, AMRFinderCommandline, PlasmidFinderCommandline,\
-    ShovillCommandline
+from src.cmd import run_cmd
 
+
+TMPDIR = str()
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 DB = os.path.join(CURRENT_DIR, 'db')
 BIN_DIR = os.path.join(CURRENT_DIR, 'bin')
@@ -13,65 +15,57 @@ BIN_DIR = os.path.join(CURRENT_DIR, 'bin')
 # trimmomatic
 ADAPTERS = os.path.join(DB, 'trimmomatic.fa')
 MIN_BQ = 3
-TRIM_OPT = f"ILLUMINACLIP:{ADAPTERS}:2:30:10 LEADING:{MIN_BQ} TRAILING:{MIN_BQ} SLIDINGWINDOW:4:20 MINLEN:36 TOPHRED33"
+TRIM_OPT = f"ILLUMINACLIP:{ADAPTERS}:2:30:10 HEADCROP:10 LEADING:{MIN_BQ} TRAILING:{MIN_BQ} SLIDINGWINDOW:4:20 " \
+           f"MINLEN:36 TOPHRED33"
 
 
-def trimming(forward_reads, reverse_reads, outdir, threads):
-    forward_paired = os.path.join(outdir, 'paired_R1.fq.gz')
-    reverse_paired = os.path.join(outdir, 'paired_R2.fq.gz')
+def trimming(forward_reads, reverse_reads, outdir, threads, compress=False):
+    extension = {True: '.fq.gz', False: '.fq'}
+    forward_paired = os.path.join(outdir, 'paired_R1' + extension[compress])
+    reverse_paired = os.path.join(outdir, 'paired_R2' + extension[compress])
     cmd = f"trimmomatic PE -threads {threads} {forward_reads} {reverse_reads} {forward_paired} /dev/null" \
           f" {reverse_paired} /dev/null {TRIM_OPT}"
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    stdout_str, stderr_str = p.communicate()
-    return_code = p.returncode
-    if return_code:
-        raise subprocess.CalledProcessError(return_code, cmd, stdout_str, stderr_str)
+    run_cmd(cmd)
     return forward_paired, reverse_paired
 
 
-def workflow(forward_reads, reverse_reads, outdir, threads):
-    logger = create_logger(os.path.join(outdir, 'pga.log'))
+def species_identify(input_files, outdir):
+    database = os.path.join(DB, 'kmerfinder_db', 'bacteria.ATG')
+    taxonomy_file = os.path.join(DB, 'kmerfinder_db', 'bacteria.tax')
+    with TemporaryDirectory(dir=TMPDIR) as tmp:
+        run_cmd(f'kmerfinder.py -i {input_files} -o {tmp} -db {database} -tax {taxonomy_file} -x')
+        shutil.copy(os.path.join(tmp, 'results.txt'), os.path.join(outdir, 'kmerfinder.txt'))
 
-    logger.info("Quality control checks.")
-    cline = FastqcCommandline(forward_reads, reverse_reads, outdir=outdir, threads=2)
-    cline()
 
-    logger.info("Species identification.")
-    tmp = TemporaryDirectory(prefix='kmerfinder_', dir=outdir)
-    tmp_dir = tmp.name
-    cline = KmerFinderCommandline(infile=" ".join((forward_reads, reverse_reads)), outdir=tmp_dir,
-                                  database=os.path.join(DB, 'kmerfinder_db', 'bacteria.ATG'),
-                                  taxonomy_file=os.path.join(DB, 'kmerfinder_db', 'bacteria.tax'),
-                                  extented_output=True)
-    cline()
-    os.renames(os.path.join(tmp_dir, 'results.txt'), os.path.join(outdir, 'kmerfinder.txt'))
+def correct_reads(forward_reads, reverse_reads, outdir, threads):
+    run_cmd(f'spades.py -1 {forward_reads} -2 {reverse_reads} -o {outdir} -t {threads} --tmp-dir {TMPDIR} '
+            f'--only-error-correction')
+    yaml_file = os.path.join(outdir, 'corrected', 'corrected.yaml')
+    with open(yaml_file) as handle:
+        data = yaml.safe_load(handle)
+    return data[0]['left reads'][0], data[0]['right reads'][0]
 
-    logger.info("Trimming reads.")
-    tmp = TemporaryDirectory(prefix='trimmomatic_', dir=outdir)
-    tmp_dir = tmp.name
-    forward_paired, reverse_paired = trimming(forward_reads, reverse_reads, tmp_dir, threads)
 
-    logger.info("Assembling reads with SPAdes.")
-    cline = ShovillCommandline(r1=forward_paired, r2=reverse_paired, outdir=outdir, depth=0, tmpdir='/dev/shm',
-                               cpus=threads, ram=32, nostitch=True, force=True)
-    cline()
+def assembly(forward_reads, reverse_reads, outdir, threads):
+    forward_trim_reads, reverse_trim_reads = trimming(forward_reads, reverse_reads, outdir, threads)
+    assembler = 'spades.py'
+    run_cmd(f'{assembler} -1 {forward_trim_reads} -2 {reverse_trim_reads} -o {outdir} --tmp-dir {TMPDIR} -t {threads} '
+            f'--careful')
 
-    contigs_file = os.path.join(outdir, 'contigs.fa')
 
-    logger.info("Plasmid type prediction.")
-    tmp = TemporaryDirectory(prefix='plasmidfinder_', dir=outdir)
-    tmp_dir = tmp.name
-    cline = PlasmidFinderCommandline(infile=contigs_file, outdir=tmp_dir,
-                                     database=os.path.join(DB, 'plasmidfinder_db'),
-                                     tmp=tmp_dir, extented_output=True)
-    cline()
-    os.renames(os.path.join(tmp_dir, 'results_tab.tsv'), os.path.join(outdir, 'plasmidfinder.txt'))
+def plasmid_identify(seqfile, outdir):
+    database = os.path.join(DB, 'plasmidfinder_db')
+    with TemporaryDirectory(dir=TMPDIR) as tmp:
+        run_cmd(f"plasmidfinder.py -i {seqfile} -o {tmp} -p {database} -tmp {tmp} -x")
+        shutil.copy(os.path.join(tmp, 'results_tab.tsv'), os.path.join(outdir, 'plasmidfinder.txt'))
 
-    logger.info("Identifies AMR genes.")
-    cline = AMRFinderCommandline(cmd=os.path.join(BIN_DIR, 'amr', 'amrfinder'),
-                                 nuc_fasta=contigs_file, output_file=os.path.join(outdir, 'amrfinder.txt'), threads=threads)
-    cline()
-    logger.info("Done.")
+
+def resistance_gene_detection(seqfile, output_file, threads):
+    database = os.path.join(DB, 'amr', 'latest')
+    with TemporaryDirectory(dir=TMPDIR) as tmp:
+        tmp_file = os.path.join(tmp, 'locus.faa')
+        run_cmd(f'prodigal -i {seqfile} -a {tmp_file}')
+        run_cmd(f'amrfinder -p {tmp_file} -o {output_file} -d {database} --threads {threads}')
 
 
 def main():
@@ -80,15 +74,35 @@ def main():
     parser.add_argument("-r2", required=True, help="file with reverse paired-end reads")
     parser.add_argument("-o", "--outdir", required=True, help="directory to store all the resulting files")
     parser.add_argument("-t", "--threads", default=8, type=int, required=False, help="number of threads [Default 8]")
+    parser.add_argument("--tmp-dir", default='/tmp', required=False,
+                        help="directory for temporary files. [Default /tmp]")
     args = parser.parse_args()
 
-    forward_reads = args.r1
-    reverse_reads = args.r2
-    outdir = args.outdir
-    threads = args.threads
+    global TMPDIR
+    TMPDIR = args.tmp_dir
 
-    os.makedirs(outdir, exist_ok=True)
-    workflow(forward_reads, reverse_reads, outdir, threads)
+    os.makedirs(args.outdir, exist_ok=True)
+    logger = create_logger(os.path.join(args.outdir, 'pga.log'))
+
+    logger.info("Quality control checks.")
+    run_cmd(f'fastqc -o {args.outdir} -t {args.threads} {args.r1} {args.r2}')
+
+    logger.info("Species identification.")
+    species_identify(f'{args.r1} {args.r2}', args.outdir)
+
+    logger.info("Assembling reads with SPAdes.")
+    with TemporaryDirectory(dir=args.outdir) as outdir:
+        assembly(args.r1, args.r2, outdir, args.threads)
+        shutil.copy(os.path.join(outdir, 'contigs.fasta'), args.outdir)
+    seqfile = os.path.join(args.outdir, 'contigs.fa')
+
+    logger.info("Plasmid type prediction.")
+    plasmid_identify(seqfile, args.outdir)
+
+    logger.info("Identifies AMR genes.")
+    output_file = os.path.join(args.outdir, 'amrfinder.txt')
+    resistance_gene_detection(seqfile, output_file, args.threads)
+    logger.info("Done.")
 
 
 if __name__ == '__main__':
